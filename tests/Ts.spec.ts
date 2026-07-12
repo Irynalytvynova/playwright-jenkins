de7b4c33b54054d5d9e127f46908c86fad9664ae
Iryna Lytvynova <li03.1989@gmail.com>
2026-07-12 20:04:17 +0300

    Error

--- tests/Ts.spec.ts
+++ tests/Ts.spec.ts
@@ -2,6 +2,6 @@ import{test, expect} from '@playwright/test'
 test('test_01_first_trial', async ({page}) => 
 {
     await page.goto('https://www.youtube.com/');
-    await page.locator('a[id="endpoint"]').getByText('Home', { exact: true }).first().click();
+    await page.locator('a[id="endpoint"]').getByText('e', { exact: true }).first().click();
 })
 
